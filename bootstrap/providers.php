1f40bac38f282e0beac2556bb37a2234781444ed
daniel <danielveronese@gmail.com>
2025-04-18 19:02:16 -0300

    remove telescope production

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,7 +5,5 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\RelationProvider::class,
     App\Providers\RouteServiceProvider::class,
-    App\Providers\StorageServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     SocialiteProviders\Manager\ServiceProvider::class,
 ];
