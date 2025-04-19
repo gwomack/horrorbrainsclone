1dafe4be0c4e179bc9565c03e09162ac1140cbab
daniel <danielveronese@gmail.com>
2025-04-19 18:21:08 -0300

    insert total page when migrating

--- database/migrations/2025_04_19_162303_create_tmdb_tables.php
+++ database/migrations/2025_04_19_162303_create_tmdb_tables.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -22,6 +23,8 @@ return new class extends Migration
             $table->unsignedSmallInteger('total');
             $table->timestamps();
         });
+
+        DB::table('tmdb_total_pages')->insert(['total' => 50000, 'created_at' => now(), 'updated_at' => now()]);
     }
 
     /**
